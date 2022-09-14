d34edff5f037d470910d2870057cf6a2a9c2b326
Max <3505918836@qq.com>
2022-09-14 20:22:53 +0800

    chuck

--- lodash/max0630-lodash.js
+++ lodash/max0630-lodash.js
@@ -1,6 +1,43 @@
 var max0630 = {
 
-  chunk: function () { },
+  chunk: function (array, size = 1) {
+
+    var newAry = []
+    var k = 0
+    while (k < array.length) {
+      var partAry = []
+      while (k < size) {
+        if (array.length == 0) {
+          break
+        }
+        partAry.push(array[0])
+        array.shift()
+        k++
+      }
+      newAry.push(partAry)
+      k = 0
+    }
+    return newAry
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   compact: function () { },
 
